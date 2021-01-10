a91d212835105f7b3d71fee5e33aef7a2f56c0cd
Hanseunghoon <3246902@naver.com>
2021-01-10 17:05:27 +0900

    Navigation and Redirecting

--- src/App.js
+++ src/App.js
@@ -1,57 +1,20 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './movies';
-import "./movie.css"
-import "./App.css"
+import About from './routes/about'
+import Home from './routes/home'
+import Detail from './routes/detail'
+import Navigation from './components/navigation'
+import {HashRouter, Route} from "react-router-dom";
+import './App.css';
 
-class App extends React.Component {
-
-  state = {
-    isLoading: true,
-    movies: []
-  }
-
-  getMovies = async() =>{    
-    const {data : {data : {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
-    this.setState({movies:movies, isLoading:false});
-  }
-
-  async componentDidMount(){
-    this.getMovies();
-  }
-
-  render()
-  {
-    const {isLoading, movies} = this.state;
-
-    return (
-      <section className="container">
-        {isLoading ?  
-          (
-            <div className="loader">
-              <span className="loader-text">Loading...</span>
-            </div>
-          )
-          : 
-          (
-            <div className="movies">
-              {movies.map(movie => (
-                <Movie 
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              ))}
-            </div>
-          )
-        }
-      </section>
-    )
-  }
+function App(){
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/movie/:id" component={Detail}/>
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/detail.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Detail extends React.Component {
+
+  componentDidMount(){
+    
+    const {location, history} = this.props;
+    
+    if(location.state === undefined){
+      // If i have not 'location', Redirect to Home
+      history.push("/");
+    }
+  }
+
+  render() {
+    
+    const {location} = this.props;
+
+    if(location.state){
+      return (
+        <span>{location.state.title}</span>
+      );
+    }
+    else{
+      return null;
+    }
+  }
+}
+
+export default Detail;
