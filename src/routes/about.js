ad1bdd0f86c9fc7f538da6fab9280c9beef5f5be
Hanseunghoon <3246902@naver.com>
2021-01-13 00:03:24 +0900

    refactor code js

--- src/routes/about.js
+++ src/routes/about.js
@@ -7,14 +7,10 @@ function About(props){
     <div className="about__container">
       <span>
         “It is a website where you can see information about movies that many audiences have visited and movies that i have enjoyed deeply.”
-
         <br/><br/>
-
         Thank you for watching my web site :)
       </span>
-
       <br/><br/>
-
       <strong>Hanseunghoon, 1996</strong>
     </div>
   );
--- src/routes/detail.js
+++ src/routes/detail.js
@@ -20,22 +20,16 @@ class Detail extends React.Component {
     if(location.state){
       return (
         <section className="datail">
-
           <div className="container">
-
             <div className="row">
-
               <div className="col-12">
-
                 <div className="detail-card">
                   <img src={location.state.poster} alt={location.state.title} title={location.state.title}></img>
                   <div className="detail-data">
-
                     <div className="detail-div">
                       <h3 className="detail-title">{location.state.title}</h3>
                       <p className="detail-year">({location.state.year})</p>
                     </div>
-
                     <ul className="detail-genres">
                       {location.state.genres.map((genre, index) => (
                         <li key={index} className="detail-genre">{genre}</li>
@@ -44,12 +38,8 @@ class Detail extends React.Component {
                     <p className="detail-summary">{location.state.summary}</p>
                   </div>
                 </div>
-
               </div>
-
             </div>
-
-
           </div>
 
         </section>
